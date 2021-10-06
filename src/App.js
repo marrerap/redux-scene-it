9003f8c765422262e9ae6d6d544424f84b1e0aa7
marrerap <andrew.marrero@outlook.com>
2021-10-06 07:30:49 -0500

    added comments to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+// import Browser router as router for shorthand and add route switch and redirect
 import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
+// import pages components so the app.js knows what to render for which routes
 import SearchPage from './pages/SearchPage';
 import FavoritesPage from './pages/FavoritesPage';
 import SearchFavoritesPage from './pages/SearchFavoritesPage'
 
 function App() {
   return (
+    // wrap all components in router to allow the rendering of pages without having 
+    // to reload page.
     <Router>
     <div className="App">
+      {/* use a switch to let the router know which component to render */}
     <Switch>
+      {/* use a route tag to designate the specific path in the url for 
+      the wrapped component */}
       <Route exact path="/" >
         <SearchPage />
       </Route>
